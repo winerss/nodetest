ed236ff5adefd1ff5c167a0d78868856929a5e19
winerss <2857505905@qq.com>
2018-03-27 22:53:48 +0800

    vuex例子

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 // 定义store
-let store = new Vuex.Store({
-  state: {
-    count: 10 // 定义一个状态
-  },
-  mutations: {
-    updateCount (state, payload) {
-      state.count += payload.add
-    }
-  }
+let state = {
+  count: 10 // 定义一个状态
+}
+export default new Vuex.Store({
+  state,
+  mutations
 })
-export default store
---
+++ src/store/mutations.js
@@ -0,0 +1,8 @@
+
+// 定义store
+let mutations = {
+  updateCount (state, payload) {
+    state.count += payload.add
+  }
+}
+export default mutations
